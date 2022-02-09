92a0af9d45ba698406f662f3e12b86f27eec9e04
Jhay <61503572+Jhayokereke@users.noreply.github.com>
2022-02-09 17:18:52 +0100

    at end of class

---
+++ GroceryStore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GroceryStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Ije garri") { Price = 3000m };
+            var id = product.Id;
+
+            var store = new Store();
+            store.Products.Add(product);
+
+            CheckOut ch = new SeasonCheckOut(store, "Merry Christmas");
+
+            var newPrice = ch.CalculateDiscount(id, 0.05);
+
+
+            Console.WriteLine("ChristmasCheckout discount: {0}", newPrice);
+            ////////////////////////////////
+            Product prod = new Product("Ije garri") { Price = 3000m };
+            var i = prod.Id;
+
+            var str = new Store();
+            str.Products.Add(prod);
+
+            var c = new CheckOut(str);
+
+            var nP = c.CalculateDiscount(i, 0.05);
+
+            Console.WriteLine("Checkout discount: {0}", nP);
+        }
+    }
+}
